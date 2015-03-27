d21885a57b5758a6a85ad05d7a2216a8b6e06700
mayanez <miguel@yanez.us>
2015-03-27 16:12:11 -0400

    Adds Types header

---
+++ sw_emulation/Types.h
@@ -0,0 +1,21 @@
+#define DISPLAY_WIDTH 480
+#define DISPLAY_HEIGHT 640
+
+typedef struct {
+    int x;
+    int y;
+    int id;
+} sprite_info_t;
+
+typedef struct {
+    int r;
+    int g;
+    int b;
+} rgb_pixel_t;
+
+typedef struct {
+    int id;
+    rgb_pixel_t[8][8] pixels;
+} sprite_t;
+
+
