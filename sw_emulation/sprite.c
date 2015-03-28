728f7065a0f33ddc3ac928d9a8f48eb480d0f9eb
shrutir25 <shrutir25@hotmail.com>
2015-03-28 16:06:46 -0400

    Update sprite.c

--- sw_emulation/sprite.c
+++ sw_emulation/sprite.c
@@ -37,10 +37,24 @@ void gl_state_input (sprite_info_t Gl_array[]){
         ycoord = Gl_array[i].y;
         type = Gl_array[i].id;
         
-        for(int j = 0; j< 8; j++){
-            memset(Sprite_Array[j].r,255,8 );
-            memset(Sprite_Array[j].g,0,8 );
+        for(int j = i; j< 8+i; j++){
+            if(type == 50){
+            memset(Sprite_Array[j].r,0,8 );
+            memset(Sprite_Array[j].g,255,8 );
             memset(Sprite_Array[j].b,0,8 );
+            }
+            
+            else if (type == 49){
+                memset(Sprite_Array[j].r,255,8 );
+                memset(Sprite_Array[j].g,0,8 );
+                memset(Sprite_Array[j].b,0,8 );
+            }
+            
+            else{
+                memset(Sprite_Array[j].r,255,8 );
+                memset(Sprite_Array[j].g,255,8 );
+                memset(Sprite_Array[j].b,0,8 );
+            }
         }
 
     }
