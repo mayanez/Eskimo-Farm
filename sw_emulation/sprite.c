e2395af21df1b7290bb9a2c3d33147dba20e5dcf
shrutir25 <shrutir25@hotmail.com>
2015-04-01 20:31:43 -0400

    Updated readppm for sky and eskimo

--- sw_emulation/sprite.c
+++ sw_emulation/sprite.c
@@ -22,24 +22,25 @@ rgb_pixel_t Sprite_Array [DH][DW];
 void read_ppm(int, int, int);
 
 void sprite_init() {
-   memset(Sprite_Array, 0, DW * DH * sizeof(rgb_pixel_t));
+    read_ppm(1,0,0);
+    //memset(Sprite_Array, 0, DW * DH * sizeof(rgb_pixel_t));
 }
 
 void gl_state_input (sprite_info_t Gl_array[]){
-
+    
     int i,j,m;
     int type;
     int xcoord;
     int ycoord;
-
+    
     for (i = 0; i < INPUT_STRUCT_LENGTH; i++){
         xcoord = Gl_array[i].x;
         ycoord = Gl_array[i].y;
         type = Gl_array[i].id;
         if(type == ESKIMO){
-             read_ppm(50, xcoord, ycoord);
+            read_ppm(50, xcoord, ycoord);
         }
-             
+        
     }
 }
 
@@ -51,42 +52,48 @@ rgb_pixel_t vga_rgb_req(int hcount, int vcount){
 }
 
 void read_ppm(int id, int x, int y) {
-
+    
     char buf[2048]; //Max width size 640 columns * 3bytes per column
     unsigned int w, h, d;
     int i,j;
+    
 
     FILE *file;
-    file = fopen("50.ppm", "r");
+    if(id == 1)
+        file = fopen("1.ppm","r");
+    else if (id == 50)
+        file = fopen("50.ppm", "r");
+    else
+        printf("Invalid Id");
     
     if (file == NULL) {
-      return;
+        return;
     }
-
+    
     /* Reads format line. ex: P6 */
     fgets(buf, 256, file);
-//    debug_print("format %s\n", buf);
+    //    debug_print("format %s\n", buf);
     
     /* Reads dimensions */
     fgets(buf, 256, file);
     sscanf(buf, "%u %u", &w, &h);
- //   debug_print("w: %u h: %u\n", w, h);
-
+    //   debug_print("w: %u h: %u\n", w, h);
+    
     /* Reads the color */
     fscanf(file, "%u", &d);
-
+    
     /* Move past last newline char to image start */
     fseek(file, 1, SEEK_CUR);
-
+    
     size_t rd = 0;
     for (i = y; i < y + h && i < DH; i++){
-      for(j = x; j < x + w && j < DW; j++){
-	  rd = fread(buf, 3, 1, file);
-          Sprite_Array[i][j].r = buf[0];
-          Sprite_Array[i][j].g = buf[1];
-          Sprite_Array[i][j].b = buf[2];
-      }
+        for(j = x; j < x + w && j < DW; j++){
+            rd = fread(buf, 3, 1, file);
+            Sprite_Array[i][j].r = buf[0];
+            Sprite_Array[i][j].g = buf[1];
+            Sprite_Array[i][j].b = buf[2];
+        }
     }
-   
-   fclose(file);
+    
+    fclose(file);
 }
