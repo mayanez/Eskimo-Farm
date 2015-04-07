9702e73102e1703d610596b704b8410f1d5a0c40
may2114 <may2114@micro12.ilab.columbia.edu>
2015-04-07 12:26:03 -0400

    Fixes Module signature

--- hw/Sprite_Controller.sv
+++ hw/Sprite_Controller.sv
@@ -10,9 +10,7 @@ module SPRITE_CONTROLLER(  input logic clk,
                            input logic [23:0] gl_array [19:0],
                            input logic [9:0]  VGA_HCOUNT, VGA_VCOUNT,
                            input logic [2:0]  address,
-                           output logic [7:0] VGA_R, VGA_G, VGA_B,
-                           output logic       VGA_CLK, VGA_HS, VGA_VS, VGA_BLANK_n,
-                           output logic       VGA_SYNC_n);
+                           output logic [7:0] VGA_R, VGA_G, VGA_B);
 
    /* At least 9 bits for 480 and at least 10 bits for 640 and 5bits for type*/
    /* 20 sprite entry array */
