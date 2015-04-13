b7beac1c29805482d9a6eed9d58341131be04870
Miguel A. Yanez <miguel@yanez.us>
2015-04-13 12:31:24 -0400

    Working. Cleaning up code

--- hw/Sprite_Controller.sv
+++ hw/Sprite_Controller.sv
@@ -7,33 +7,37 @@
 
 module Sprite_Controller(  input logic clk,
                            input logic reset,
-                           input logic [31:0] gl_array,
+                           input logic [23:0] sprite1,
                            input logic [9:0]  VGA_HCOUNT, VGA_VCOUNT,
 						   input wire [11:0] M_moon,
-						   output logic [14:0] addr_t,
+						   output logic [14:0] addr_moon,
                            output logic [7:0] VGA_R, VGA_G, VGA_B);
 
    /* At least 9 bits for 480 and at least 10 bits for 640 and 5bits for id*/
    /* 20 sprite entry array */
 
-   reg [11:0] line_buffer [639:0];
+   logic [11:0] line_buffer [639:0];
 
-   logic [11:0] M_temp;
 
    logic [8:0] x11, x12;
-   logic [9:0] y11,y12;
+   logic [9:0] y11, y12;
    logic [4:0] id1;
-   reg buf_count;
 
-   assign x11 = 0;
-   assign x12 = 9'd64;
-   assign y11 = 0;
-   assign y12 = 10'd64;
+   assign y11 = 0;//sprite1[9:0];
+   assign x11 = 0;//sprite1[18:10];
+   assign id1 = 1;//sprite1[23:0];
+
+   always_ff@(posedge clk) begin
+     if (id1 == 5'd1) begin
+       x12 <= x11 + 9'd64; /* Add dimensions of sprite corresponding to id1 */
+       y12 <= y11 + 10'd64;
+     end
+   end
 
    logic sprite1_on;
-
+   
    assign sprite1_on = (VGA_VCOUNT >= y11 && VGA_VCOUNT <= y12 && VGA_HCOUNT >= x11 && VGA_HCOUNT <= x12) ? 1 : 0;
-   assign addr_t = VGA_VCOUNT*64 + VGA_HCOUNT;
+   assign addr_moon = VGA_VCOUNT*64 + VGA_HCOUNT;
 
    always@(posedge clk)
     begin
--- hw/VGA_LED.sv
+++ hw/VGA_LED.sv
@@ -7,26 +7,28 @@
 
 module VGA_LED(input logic        clk,
 	       input logic 	  reset,
-	       input logic [31:0] gl_array, /*32-bits due to iowrite32 */
+	       input logic [511:0] gl_input, /* 20 * 24-bit entries rounded up to closest power of 2 */
 	       input logic 	  write,
-	       input 		  chipselect,
-	       input logic [2:0]  address,
 	       output logic [7:0] VGA_R, VGA_G, VGA_B,
 	       output logic 	  VGA_CLK, VGA_HS, VGA_VS, VGA_BLANK_n,
 	       output logic 	  VGA_SYNC_n);
 
-   wire [9:0] VGA_HCOUNT;
-   wire [9:0] VGA_VCOUNT;
-   wire VGA_CLOCK;
+   logic [23:0] gl_array [19:0]; 
+   logic [9:0] VGA_HCOUNT;
+   logic [9:0] VGA_VCOUNT;
+   logic VGA_CLOCK;
 
-   wire [14:0] addr_t;
-   wire [11:0] M_moon;
+   assign gl_array[0] = gl_input[23:0];
+   /* Continue until gl[19] */
 
-   moon mn(.clock(VGA_CLK), .address(addr_t), .q(M_moon));
+   /* One block like this for every sprite we have available */
+   logic [14:0] addr_moon; /* The #bits varies on dimensions of sprite */
+   logic [11:0] M_moon; /* Change to 24-bit color */
+   moon mn(.clock(VGA_CLK), .address(addr_moon), .q(M_moon));
+   
 
-   VGA_LED_Emulator led_emulator(.clk50(clk), .reset(reset), .VGA_HCOUNT(VGA_HCOUNT), .VGA_VCOUNT(VGA_VCOUNT), .VGA_CLK(VGA_CLK), .VGA_HS(VGA_HS), .VGA_VS(VGA_VS), .VGA_BLANK_n(VGA_BLANK_n), .VGA_SYNC_n(VGA_SYNC_n));
-
-   Sprite_Controller sprite_controller(.clk(VGA_CLK), .reset(reset), .gl_array(gl_array), .VGA_HCOUNT(VGA_HCOUNT), .VGA_VCOUNT(VGA_VCOUNT), .VGA_R(VGA_R), .VGA_G(VGA_G), .VGA_B(VGA_B), .addr_t(addr_t), .M_moon(M_moon));
+   VGA_LED_Emulator led_emulator(.clk50(clk), .*);
+   Sprite_Controller sprite_controller(.clk(VGA_CLK), .sprite1(gl_array[0]), .*);
 
 endmodule
 
