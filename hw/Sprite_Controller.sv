a57fe93d322d76810dea3fd4723d0d4e403300bf
Miguel A. Yanez <miguel@yanez.us>
2015-04-15 22:23:58 -0400

    Adds 3 sprites + rom

--- hw/Sprite_Controller.sv
+++ hw/Sprite_Controller.sv
@@ -7,10 +7,10 @@
 
 module Sprite_Controller(  input logic clk,
                            input logic reset,
-                           input logic [23:0] sprite1, sprite2, sprite3,
+                           input logic [24:0] sprite1, sprite2, sprite3,
                            input logic [9:0]  VGA_HCOUNT, VGA_VCOUNT,
-						   input wire [23:0] M_sprite1, M_sprite2,
-						   output logic [9:0] addr_sprite1, addr_sprite2,
+						   input wire [23:0] M_sprite1, M_sprite2, M_sprite3, 
+						   output logic [9:0] addr_sprite1, addr_sprite2, addr_sprite3,
                            output logic [7:0] VGA_R, VGA_G, VGA_B);
 
    /* At least 9 bits for 480 and at least 10 bits for 640 and 5bits for id*/
@@ -21,19 +21,41 @@ module Sprite_Controller(  input logic clk,
    logic [23:0] M_buf;
 
    logic buf_toggle;
+   logic [9:0] x11, y11, x12, y12, x21, y21, x22, y22, x31, y31, x32, y32;
 
+   assign id1 = sprite1[24:20];
+   assign x11 = sprite1[9:0];
+   assign y11 = sprite1[19:10];
+   assign x12 = (id1 == 5'd1 || id1 == 5'd2 || id1 == 5'd3) ? 10'd31 : 0;  /* For all id of same size */
+   assign y12 = (id1 == 5'd1 || id1 == 5'd2 || id1 == 5'd3) ? 10'd31 : 0;
 
-   assign sprite1_on = VGA_VCOUNT >= sprite1[9:0] && VGA_VCOUNT <= (sprite1[9:0] + 10'd31) && VGA_HCOUNT >= sprite1[18:10] && VGA_HCOUNT <= (sprite1[18:10] + 10'd31);
-   assign sprite2_on = VGA_VCOUNT >= sprite2[9:0] && VGA_VCOUNT <= (sprite2[9:0] + 10'd31) && VGA_HCOUNT >= sprite2[18:10] && VGA_HCOUNT <= (sprite2[18:10] + 10'd31);
+   assign id2 = sprite2[24:20];
+   assign x21 = sprite2[9:0];
+   assign y21 = sprite2[19:10];
+   assign x22 = (id2 == 5'd1 || id2 == 5'd2 || id2 == 5'd3) ? 10'd31 : 0;
+   assign y22 = (id2 == 5'd1 || id2 == 5'd2 || id2 == 5'd3) ? 10'd31 : 0;
 
-   assign addr_sprite1 = (VGA_HCOUNT - sprite1[18:10]) + ((VGA_VCOUNT + 1 - sprite1[9:0]) << 5);
-   assign addr_sprite2 = (VGA_HCOUNT - sprite2[18:10]) + ((VGA_VCOUNT + 1 - sprite2[9:0]) << 5);
+   assign id3 = sprite3[24:20];
+   assign x31 = sprite3[9:0];
+   assign y31 = sprite3[19:10];
+   assign x32 = (id3 == 5'd1 || id3 == 5'd2 || id3 == 5'd3) ? 10'd31 : 0;
+   assign y32 = (id3 == 5'd1 || id3 == 5'd2 || id3 == 5'd3) ? 10'd31 : 0;
+
+   assign sprite1_on = VGA_VCOUNT >= y11 && VGA_VCOUNT <= y12 && VGA_HCOUNT >= x11 && VGA_HCOUNT <= x12;
+   assign sprite2_on = VGA_VCOUNT >= y21 && VGA_VCOUNT <= y22 && VGA_HCOUNT >= x21 && VGA_HCOUNT <= x22;
+   assign sprite3_on = VGA_VCOUNT >= y31 && VGA_VCOUNT <= y32 && VGA_HCOUNT >= x31 && VGA_HCOUNT <= x32;
+
+   assign addr_sprite1 = (VGA_HCOUNT - x11) + ((VGA_VCOUNT + 1 - y11) << 5);
+   assign addr_sprite2 = (VGA_HCOUNT - x21) + ((VGA_VCOUNT + 1 - y21) << 5);
+   assign addr_sprite3 = (VGA_HCOUNT - x31) + ((VGA_VCOUNT + 1 - y31) << 5);
 
    always @(*) begin
       if (sprite1_on)
          M_buf = M_sprite1;
       else if (sprite2_on)
          M_buf = M_sprite2;
+	  else if (sprite3_on)
+         M_buf = M_sprite3;
       else
          M_buf = 0;
    end
--- hw/VGA_LED.sv
+++ hw/VGA_LED.sv
@@ -13,32 +13,43 @@ module VGA_LED(input logic        clk,
 	       output logic 	  VGA_CLK, VGA_HS, VGA_VS, VGA_BLANK_n,
 	       output logic 	  VGA_SYNC_n);
 
-   logic [23:0] gl_array [19:0]; 
+   logic [24:0] gl_array [19:0]; 
    logic [9:0] VGA_HCOUNT;
    logic [9:0] VGA_VCOUNT;
    logic [4:0] id1;
-   logic [23:0] sprite1,sprite2,sprite3,sprite4,sprite5,sprite6,sprite7,sprite8,sprite9,sprite10,sprite11,sprite12,sprite13,sprite14,sprite15,sprite16,sprite17,sprite18,sprite19,sprite20;
+   logic [24:0] sprite1,sprite2,sprite3,sprite4,sprite5,sprite6,sprite7,sprite8,sprite9,sprite10,sprite11,sprite12,sprite13,sprite14,sprite15,sprite16,sprite17,sprite18,sprite19,sprite20;
    logic VGA_CLOCK;
    
   always_ff@(posedge clk) begin
       if (write) begin
-	   gl_array[0] <= gl_input[23:0];
-           gl_array[1] <= gl_input[45:24];
+	   gl_array[0] <= gl_input[24:0];
+       gl_array[1] <= gl_input[46:25];
+       gl_array[2] <= gl_input[69:47];
       end
    end
 
    assign sprite1 = gl_array[0];
    assign sprite2 = gl_array[1];
+   assign sprite3 = gl_array[2];
 
-   logic [9:0] addr_sprite1, addr_sprite2;
-   logic [23:0] M_sprite1, M_sprite2, M_ship, M_pig;
+   assign id1 = sprite1[24:20];
+   assign id2 = sprite2[24:20];
+   assign id3 = sprite3[24:20];
+
+   logic [9:0] addr_sprite1, addr_sprite2, addr_sprite3;
+   logic [23:0] M_sprite1, M_sprite2, M_sprite3, M_ship, M_pig, M_bee;
 
    ship sm(.clock(VGA_CLK), .address(addr_sprite1), .q(M_ship));
    pig  pg(.clock(VGA_CLK), .address(addr_sprite2), .q(M_pig));
+   bee  be(.clock(VGA_CLK), .address(addr_sprite3), .q(M_bee));
 
 
-   assign M_sprite1 = M_ship;
-   assign M_sprite2 = M_pig;
+   assign M_sprite1 = (id1 == 5'd1) ? M_ship : 0;
+   
+   assign M_sprite2 = (id2 == 5'd2) ? M_pig : 0;
+
+   assign M_sprite3 = (id3 == 5'd3) ? M_bee : 0;
+
 
    VGA_LED_Emulator led_emulator(.clk50(clk), .*);
    Sprite_Controller sprite_controller(.clk(VGA_CLK), .*);
