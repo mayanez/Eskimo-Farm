f565bb9a2fb39925d2de0e4c10bf1aa90852b2c6
shuklaprachi21 <shuklaprachi21@gmail.com>
2015-04-22 00:32:14 -0400

    Update vga_led.h

--- sw/vga_led.h
+++ sw/vga_led.h
@@ -6,8 +6,10 @@
 #define MAX_SPRITES 20
 #define MAX_SPRITE_DIM 128
 #define MAX_SPRITE_ID 40 /* Unique number of sprites == # of ROMs */
-#define MAX_X 640
-#define MAX_Y 480
+#define MIN_X 0
+#define MIN_Y 31
+#define MAX_X 607
+#define MAX_Y 447
 
 #define SHIP_ID 1
 #define PIG_ID 	2
