6e485f53adb022ec7ecc83c7f1830a2621c4b414
shuklaprachi21 <shuklaprachi21@gmail.com>
2015-04-21 22:22:14 -0400

    Update game.h

--- sw/game.h
+++ sw/game.h
@@ -1,6 +1,7 @@
 #include "vga_led.h"
 
 #define MAX_BULLETS 10
+#define MAX_ENEMIES 20
 
 enum direction_t {up, down, left, right, stationary};
 enum state_t {menu, game, game_over};
@@ -8,9 +9,26 @@ enum state_t {menu, game, game_over};
 typedef struct {
 	sprite_t sprite_info;
 	unsigned int lives;
+	SDL_Rect hitbox;
 } player_t;
 
 typedef struct {
     sprite_t sprite_info;
-    unsigned int alive;
+    unsigned int alive; 
+ 	SDL_Rect hitbox;   
 } bullet_t;
+
+typedef struct {
+	sprite_t sprite_info;
+	unsigned int alive = 0 ; //can be re-allotted to another sprite
+	unsigned int points;
+	enum direction_t direction;
+	SDL_Rect hitbox;
+	/*
+		int speed;
+	int state;
+	int state_speed;
+	Uint32 state_time;
+*/
+
+}enemy_t; 
