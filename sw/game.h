0b966ab079c734afd444b0757c784029e2bf0d50
shrutir25 <shrutir25@hotmail.com>
2015-04-28 00:23:56 -0400

    Update game.h
    Added Goat,Frog and Chick

--- sw/game.h
+++ sw/game.h
@@ -2,16 +2,22 @@
 
 #define MAX_BULLETS 5
 #define BULLET_SPEED 20
-#define MAX_ENEMIES 10
+#define MAX_ENEMIES 15
 #define PLAYER_STEP_SIZE 2
 #define MAX_CLOUDS 3
 #define MAX_LIVES 3
 #define PIG_SCORE 16
 #define BEE_SCORE 40
 #define COW_SCORE 60
+#define FROG_SCORE 80
+#define GOAT SCORE 100
+#define CHICK SCORE 120
 #define PIG_SPEED 1
 #define BEE_SPEED 2
 #define COW_SPEED 3
+#define FROG_SPEED 3
+#define GOAT_SPEED 3
+#define CHICK_SPEED 3
 
 enum direction_t {up, down, left, right, stationary};
 enum state_t {menu, game, game_over};
