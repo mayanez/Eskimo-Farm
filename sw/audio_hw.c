bb44f531a67134a88f0240eb97329f0e7f84f4c2
Miguel A. Yanez <miguel@yanez.us>
2015-05-03 19:43:52 -0400

    Adds Audio Driver + Modifies VGA Driver with new dim bitlength

--- sw/audio_hw.c
+++ sw/audio_hw.c
@@ -32,14 +32,14 @@
 #include <linux/of_address.h>
 #include <linux/fs.h>
 #include <linux/uaccess.h>
-#include "vga_led.h"
+#include "audio_hw.h"
 
-#define DRIVER_NAME "vga_led"
+#define DRIVER_NAME "audio_hw"
 
 /*
  * Information about our device
  */
-struct vga_led_dev {
+struct audio_hw_dev {
 	struct resource res; /* Resource: our registers */
 	void __iomem *virtbase; /* Where registers can be accessed in memory */
 } dev;
@@ -50,40 +50,18 @@ struct vga_led_dev {
  * Read or write the segments on single digits.
  * Note extensive error checking of arguments
  */
-static long vga_led_ioctl(struct file *f, unsigned int cmd, unsigned long arg)
+static long audio_hw_ioctl(struct file *f, unsigned int cmd, unsigned long arg)
 {
-	sprite_t sprite;
-    unsigned int vga_ready;
+    unsigned int control;
 
 	switch (cmd) {
-		case VGA_SET_SPRITE:
-			if (copy_from_user(&sprite, (sprite_t *) arg, sizeof(sprite_t)))
+		case AUDIO_SET_CONTROL:
+			if (copy_from_user(&control, (unsigned int *) arg, sizeof(unsigned int)))
 				return -EACCES;
-
-			if (sprite.dim < 0 || sprite.dim > MAX_SPRITE_DIM ||
-			    sprite.id < 0 || sprite.id > MAX_SPRITE_ID ||
-			    sprite.y < 0 || sprite.y > MAX_Y || 
-				sprite.x < 0 || sprite.x > MAX_X ||
-			    sprite.s < 0 || sprite.s > MAX_SPRITES)
-					return -EINVAL;
-
-			iowrite32(((sprite.dim & 0x7F) << 26) | 
-					  ((sprite.id & 0x1F) << 20)  |
-					  ((sprite.y & 0x3FF) << 10)  |
-					   (sprite.x & 0x3FF), dev.virtbase + (sprite.s << 2));
-
+			iowrite32(control, dev.virtbase);
 			break;
-
-		case VGA_CLEAR:
-
-			iowrite32(NULL, dev.virtbase + (60 << 2));
-
-			break;
-
-        case VGA_READY:
-            vga_ready = ioread32(dev.virtbase + (61 << 2));
-            if (copy_to_user((unsigned int *) arg, &vga_ready, sizeof(unsigned int)))
-                return -EACCES;
+        case AUDIO_MUTE:
+			iowrite32(0, dev.virtbase);
             break;
 		default:
 			return -EINVAL;
@@ -95,26 +73,26 @@ static long vga_led_ioctl(struct file *f, unsigned int cmd, unsigned long arg)
 /* The operations our device knows how to do */
 static const struct file_operations vga_led_fops = {
 	.owner		= THIS_MODULE,
-	.unlocked_ioctl = vga_led_ioctl,
+	.unlocked_ioctl = audio_hw_ioctl,
 };
 
 /* Information about our device for the "misc" framework -- like a char dev */
-static struct miscdevice vga_led_misc_device = {
+static struct miscdevice audio_hw_misc_device = {
 	.minor		= MISC_DYNAMIC_MINOR,
 	.name		= DRIVER_NAME,
-	.fops		= &vga_led_fops,
+	.fops		= &audio_hw_fops,
 };
 
 /*
  * Initialization code: get resources (registers) and display
  * a welcome message
  */
-static int __init vga_led_probe(struct platform_device *pdev)
+static int __init audio_hw_probe(struct platform_device *pdev)
 {
 	int ret;
 
 	/* Register ourselves as a misc device: creates /dev/vga_led */
-	ret = misc_register(&vga_led_misc_device);
+	ret = misc_register(&audio_hw_misc_device);
 
 	/* Get the address of our registers from the device tree */
 	ret = of_address_to_resource(pdev->dev.of_node, 0, &dev.res);
@@ -142,55 +120,55 @@ static int __init vga_led_probe(struct platform_device *pdev)
 out_release_mem_region:
 	release_mem_region(dev.res.start, resource_size(&dev.res));
 out_deregister:
-	misc_deregister(&vga_led_misc_device);
+	misc_deregister(&audio_hw_misc_device);
 	return ret;
 }
 
 /* Clean-up code: release resources */
-static int vga_led_remove(struct platform_device *pdev)
+static int audio_hw_remove(struct platform_device *pdev)
 {
 	iounmap(dev.virtbase);
 	release_mem_region(dev.res.start, resource_size(&dev.res));
-	misc_deregister(&vga_led_misc_device);
+	misc_deregister(&audio_hw_misc_device);
 	return 0;
 }
 
 /* Which "compatible" string(s) to search for in the Device Tree */
 #ifdef CONFIG_OF
-static const struct of_device_id vga_led_of_match[] = {
-	{ .compatible = "altr,vga_led" },
+static const struct of_device_id audio_hw_of_match[] = {
+	{ .compatible = "altr,audio_hw" },
 	{},
 };
-MODULE_DEVICE_TABLE(of, vga_led_of_match);
+MODULE_DEVICE_TABLE(of, audio_hw_of_match);
 #endif
 
 /* Information for registering ourselves as a "platform" driver */
-static struct platform_driver vga_led_driver = {
+static struct platform_driver audio_hw_driver = {
 	.driver	= {
 		.name	= DRIVER_NAME,
 		.owner	= THIS_MODULE,
-		.of_match_table = of_match_ptr(vga_led_of_match),
+		.of_match_table = of_match_ptr(audio_hw_of_match),
 	},
-	.remove	= __exit_p(vga_led_remove),
+	.remove	= __exit_p(audio_hw_remove),
 };
 
 /* Called when the module is loaded: set things up */
-static int __init vga_led_init(void)
+static int __init audio_hw_init(void)
 {
 	pr_info(DRIVER_NAME ": init\n");
-	return platform_driver_probe(&vga_led_driver, vga_led_probe);
+	return platform_driver_probe(&audio_hw_driver, audio_hw_probe);
 }
 
 /* Called when the module is unloaded: release resources */
-static void __exit vga_led_exit(void)
+static void __exit audio_hw_exit(void)
 {
-	platform_driver_unregister(&vga_led_driver);
+	platform_driver_unregister(&audio_hw_driver);
 	pr_info(DRIVER_NAME ": exit\n");
 }
 
-module_init(vga_led_init);
-module_exit(vga_led_exit);
+module_init(audio_hw_init);
+module_exit(audio_hw_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Miguel A. Yanez, Columbia University");
-MODULE_DESCRIPTION("VGA Sprite Controller Driver");
+MODULE_DESCRIPTION("Audio Hardware Driver");
---
+++ sw/audio_hw.h
@@ -0,0 +1,16 @@
+#ifndef _AUDIO_HW_H
+#define _AUDIO_HW_H
+
+//#include <linux/ioctl.h>
+
+#define BACKGROUND_TRACK 1
+
+
+
+#define AUDIO_HW_MAGIC 'q'
+
+/* ioctls and their arguments */
+#define AUDIO_SET_CONTROL _IOW(AUDIO_HW_MAGIC, 1, unsigned int *)
+#define AUDIO_MUTE	      _IO(AUDIO_HW_MAGIC, 2)
+
+#endif
